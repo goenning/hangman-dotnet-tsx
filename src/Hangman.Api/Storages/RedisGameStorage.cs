93b1b60b8fc3b995692f19357c45b27ad0b3dfe1
goenning <oenning.ti@gmail.com>
2016-08-02 21:33:00 -0300

    add redis dependency

---
+++ src/Hangman.Api/Storages/RedisGameStorage.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Hangman.Api.Storages
+{
+    public class RedisGameStorage : IGameStorage
+    {
+        public Game Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid Store(Game game)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
